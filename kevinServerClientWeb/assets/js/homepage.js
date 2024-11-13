d7ae65014dc4cdcd5b1d076475ec8b74c5169043
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2024-11-13 02:23:46 +0000

    克隆原本的项目

---
+++ kevinServerClientWeb/assets/js/homepage.js
@@ -0,0 +1,63 @@
+const blurTitle = document.getElementById("blurTitle");
+const wellcome = document.getElementById("wellcome");
+const hiddenBox = document.getElementById("hiddenBox");
+const context = document.getElementById("context");
+
+var date = new Date();
+var clientHour = date.getHours();
+console.log(date, clientHour);
+
+var dayNight;
+//图片总数
+var twiLightBackGorundImageNum = 3;
+var dayBackGroundImageNum = 9;
+var nightBackGorundImageNum = 2;
+
+var randomBackGroundNum, randomBackGroundString;
+
+function dayNightCompare() {
+    if (clientHour >= 5 && clientHour <= 7) {//判断早晨 早上5点至7点
+        dayNight = "黄昏";
+    } else if (clientHour > 7 && clientHour <= 16) {//判断白天 早上8点至下午4点
+        dayNight = "早晨";
+    } else if (clientHour > 16 && clientHour <= 19) {//判断晚霞 下午5点至下午7点
+        dayNight = "黄昏";
+    } else {//其余为夜晚
+        dayNight = "夜晚";
+    }
+    setBackGroundImage();
+}
+function setBackGroundImage(){
+    if (dayNight == "早晨") {
+        randomBackGroundNum = Math.floor((Math.random() * dayBackGroundImageNum) + 1);
+        randomBackGroundString = "亮" + randomBackGroundNum;
+        wellcome.style.color = "#101010";
+    } else if(dayNight == "黄昏"){
+        randomBackGroundNum = Math.floor((Math.random() * twiLightBackGorundImageNum) + 1);
+        randomBackGroundString = "黄" + randomBackGroundNum;
+        wellcome.style.color = "#ffffff";
+    } else {
+        randomBackGroundNum = Math.floor((Math.random() * nightBackGorundImageNum) + 1);
+        randomBackGroundString = "暗" + randomBackGroundNum;
+        wellcome.style.color = "#ffffff";
+    }
+}
+function refreshBackGroundImage() {
+    blurTitle.style.backgroundImage = "url(\"../image/backgrounds/" + randomBackGroundString + ".jpg\"";
+    console.log(blurTitle.style.backgroundImage);
+}
+dayNightCompare();
+refreshBackGroundImage();
+
+setTimeout("hiddenBox.hidden = true", 990);
+
+blurTitle.addEventListener("pointerdown", function () {
+    setTimeout(("wellcome.hidden = true"), 990);
+    wellcome.style.animation = "disAppear 1s ease-out";
+    setTimeout(() => {
+        setTimeout(("blurTitle.hidden = true"), 990);
+        blurTitle.style.animation = "flat 1s ease-out";
+        context.hidden = false;
+        manu.style = "animation: appear 1s ease-out";
+    }, 1000);
+});
---
+++ kevinServerClientWeb/assets/js/main.js
@@ -0,0 +1,21 @@
+const manu = document.getElementById("manu");
+
+function addManu() {
+    manu.innerHTML = `
+    <div id="links">
+            <div id="navigation">
+                <p class="">导航</p>
+                <div id="subArea">
+                    <a href="contributors.html">🎈贡献者名单</a>
+                    <a href="usefulLinks.html">🔗有用的链接</a>
+                </div>
+            </div>
+        </div>
+        <a id="serverIcon" href="main.html" title="主页">
+            <img id="icon" src="../image/icons/icon.jpg" alt="主页图标">
+        </a>
+    `;
+// 以后请在这修改菜单内容 此脚本会自动为网页加上
+}
+
+addManu();
