dc3613e7b2d10792dc481dbaea17ad24d08d74d2
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2024-12-02 17:07:48 +0800

    代码格式添加/埋彩蛋

---
+++ kevinServerClientWeb/assets/js/easterEgg.js
@@ -0,0 +1,7 @@
+function show() {
+    var commit = document.getElementById("commit");
+    commit.className = "commits";
+    commit.innerHTML = "&lt;!-- 懂得都懂awa --&gt;";
+    document.getElementById("hiddenImage").innerHTML = "<br><img src=\"../image/images/runAble.jpg\" alt=\"null\"><br><p>嘿真就可以(喜</p>";
+    console.log("彩蛋已激活");
+}
