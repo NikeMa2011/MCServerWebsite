4193d192e8f26e6a6380a247ae8b08fb0c3e731f
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2024-12-02 09:38:23 +0800

    菜单样式修复

--- kevinServerClientWeb/assets/js/menu.js
+++ kevinServerClientWeb/assets/js/menu.js
@@ -4,15 +4,15 @@ function addMenu() {
     menu.innerHTML = `
     <div id="links">
             <div id="navigation">
-                <p class="">导航</p>
+                <p>导航</p>
                 <div id="subArea">
                     <a href="contributors.html">🎈贡献者名单</a>
                     <a href="usefulLinks.html">🔗有用的链接</a>
                 </div>
             </div>
         </div>
-        <a id="serverIcon" href="main.html" title="主页">
-            <img id="icon" src="../image/icons/icon.jpg" alt="主页图标">
+        <a href="main.html">
+            <img id="menuIcon" src="../image/icons/icon.jpg" alt="主页图标">
         </a>
     `;
 // 以后请在这修改菜单内容 此脚本会自动为网页加上
