2b71da7a54d6447a93293dcabb8a804abd843340
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2024-12-05 15:56:23 +0800

    修复逻辑问题

--- kevinServerClientWeb/assets/js/darkMode.js
+++ kevinServerClientWeb/assets/js/darkMode.js
@@ -1,12 +1,13 @@
-const styleTag = document.getElementById("darkModeStyle");
+const styleTag = document.getElementById("darkModeStyleTag");
 
+console.log(styleTag);
 function nightModeCompare() {
      if (clientHour >= 8 && clientHour < 17) { // Daytime
-        styleTag.innerHTML = "@import(\"../\"css/darkMode.css\")";
-        console.log("夜间模式状态: 开启");
-    } else { // Nighttime
         styleTag.innerHTML = null;
-        console.log("夜间模式状态: 关闭")
+        console.log("夜间模式状态: 关闭");
+    } else { // Nighttime
+        styleTag.innerHTML = "@import url(\"../css/nightMode.css\");";
+        console.log("夜间模式状态: 开启");
     }
 }
 nightModeCompare();
