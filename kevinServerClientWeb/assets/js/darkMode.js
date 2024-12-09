285f224b0730bf47c337a40e11de64b172e18bf9
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2024-12-09 09:43:23 +0800

    调试功能添加

--- kevinServerClientWeb/assets/js/darkMode.js
+++ kevinServerClientWeb/assets/js/darkMode.js
@@ -1,13 +1,18 @@
 const styleTag = document.getElementById("darkModeStyleTag");
 
-console.log(styleTag);
-function nightModeCompare() {
-     if (clientHour >= 8 && clientHour < 17) { // Daytime
-        styleTag.innerHTML = null;
-        console.log("夜间模式状态: 关闭");
-    } else { // Nighttime
+function nightModeCompare(op) {
+    if(!op) {
+        styleTag.innerHTML = null; 
+    } else if(op) {
         styleTag.innerHTML = "@import url(\"../css/nightMode.css\");";
-        console.log("夜间模式状态: 开启");
+    } else if(op = undefined) {
+        if(clientHour >= 8 && clientHour < 17) { // Daytime
+            styleTag.innerHTML = null;
+            console.log("夜间模式状态: 关闭");
+        } else { // Nighttime
+            styleTag.innerHTML = "@import url(\"../css/nightMode.css\");";
+            console.log("夜间模式状态: 开启");
+        }
     }
 }
 nightModeCompare();
