506e1b2aa3e7cb8392c3e2c9265f24e07cd422c6
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2024-12-09 18:01:02 +0800

    埋彩蛋

--- kevinServerClientWeb/assets/js/easterEgg.js
+++ kevinServerClientWeb/assets/js/easterEgg.js
@@ -1,7 +1,8 @@
-function show() {
+function ASCII_636f6465466f726d61742e68746d6c_easterEgg_show() {
     var commit = document.getElementById("commit");
     commit.className = "commits";
     commit.innerHTML = "&lt;!-- 懂得都懂awa --&gt;";
-    document.getElementById("hiddenImage").innerHTML = "<br><img src=\"../image/images/runAble.jpg\" alt=\"null\"><br><p>嘿真就可以(喜</p>";
+    document.getElementById("hiddenImage_0").innerHTML = "<br><img src=\"../image/images/runAble.jpg\" alt=\"null\"><br><p>嘿真就可以(喜</p>";
+    document.getElementById("hiddenImage_1").innerHTML = "<br><img src=\"../image/images/strange.jpg\" alt=\"null\"><br><p>奇怪的东西</p>";
     console.log("彩蛋已激活");
 }
