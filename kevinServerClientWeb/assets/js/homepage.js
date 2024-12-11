60b7f0f5f0c63b5888a44032f76d08e34b438bf2
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2024-12-11 18:54:15 +0800

    在主菜单添加判断是否刷新算法

--- kevinServerClientWeb/assets/js/homepage.js
+++ kevinServerClientWeb/assets/js/homepage.js
@@ -1,61 +1,75 @@
-const blurTitle = document.getElementById("blurTitle");
-const welcome = document.getElementById("welcome");
-const hiddenBox = document.getElementById("hiddenBox");
-const context = document.getElementById("context");
+const title = document.getElementById("title");
 
-const backgroundImageCounts = {
-    day: 9,
-    twilight: 3,
-    night: 2,
-};
-function setBackGroundImage() {
-    let randomNum;
-    let prefix;
-    
-    switch (dayNight) {
-        case "早晨":
-        case "白天":
-            randomNum = Math.floor(Math.random() * backgroundImageCounts.day) + 1;
-            prefix = "亮";
-            welcome.style.color = "#101010";
-            break;
-        case "黄昏":
-            randomNum = Math.floor(Math.random() * backgroundImageCounts.twilight) + 1;
-            prefix = "黄";
-            welcome.style.color = "#ffffff";
-            break;
-        default:
-            randomNum = Math.floor(Math.random() * backgroundImageCounts.night) + 1;
-            prefix = "暗";
-            welcome.style.color = "#ffffff";
-            break;
+if (!sessionStorage.getItem("Loaded")) {//是否刷新判断在前面
+    title.innerHTML = `
+    <div id="hiddenBox"></div>
+    <div id="blurTitle">
+        <p id="welcome" class="middle">欢迎来到 MIS Minecraft 服务器 =D</p>
+    </div>
+    `;
+    sessionStorage.setItem("Loaded", true);
+    core();
+}
+console.log("页面是否打开过: " + sessionStorage.getItem("Loaded"));
+function core() {
+    const blurTitle = document.getElementById("blurTitle");
+    const welcome = document.getElementById("welcome");
+    const hiddenBox = document.getElementById("hiddenBox");
+    const context = document.getElementById("context");
+    const backgroundImageCounts = {
+        day: 9,
+        twilight: 3,
+        night: 2,
+    };
+    function setBackGroundImage() {
+        let randomNum;
+        let prefix;
+
+        switch (dayNight) {
+            case "早晨":
+            case "白天":
+                randomNum = Math.floor(Math.random() * backgroundImageCounts.day) + 1;
+                prefix = "亮";
+                welcome.style.color = "#101010";
+                break;
+            case "黄昏":
+                randomNum = Math.floor(Math.random() * backgroundImageCounts.twilight) + 1;
+                prefix = "黄";
+                welcome.style.color = "#ffffff";
+                break;
+            default:
+                randomNum = Math.floor(Math.random() * backgroundImageCounts.night) + 1;
+                prefix = "暗";
+                welcome.style.color = "#ffffff";
+                break;
+        }
+        return `${prefix}${randomNum}`;
     }
-    return `${prefix}${randomNum}`;
-}
-function refreshBackGroundImage(imageString) {
-    blurTitle.style.backgroundImage = `url("../image/backgrounds/${imageString}.jpg")`;
-    console.log("背景图片设置为:", blurTitle.style.backgroundImage);
-}
-function hideElement(element, delay = 990) {
-    setTimeout(() => {
-        element.hidden = true;
-    }, delay);
-}
-const randomBackGroundString = setBackGroundImage();
-refreshBackGroundImage(randomBackGroundString);
-hideElement(hiddenBox);
+    function refreshBackGroundImage(imageString) {
+        blurTitle.style.backgroundImage = `url("../image/backgrounds/${imageString}.jpg")`;
+        console.log("背景图片设置为:", blurTitle.style.backgroundImage);
+    }
+    function hideElement(element, delay = 990) {
+        setTimeout(() => {
+            element.hidden = true;
+        }, delay);
+    }
+    const randomBackGroundString = setBackGroundImage();
+    refreshBackGroundImage(randomBackGroundString);
+    hideElement(hiddenBox);
 
-blurTitle.addEventListener("pointerdown", function () {
-    welcome.style.animation = "disAppear 1s ease-out";
+    blurTitle.addEventListener("pointerdown", function () {
+        welcome.style.animation = "disAppear 1s ease-out";
 
-    setTimeout(() => {
-        blurTitle.innerHTML = null;
-    }, 990);
-    setTimeout(() => {
-        blurTitle.style.animation = "flat 1s ease-out";
-        context.hidden = false;
-        menu.style.animation = "appear 1s ease-out";
-        menu.hidden = false;
-        hideElement(blurTitle);
-    }, 990);
-});
+        setTimeout(() => {
+            blurTitle.innerHTML = null;
+        }, 990);
+        setTimeout(() => {
+            blurTitle.style.animation = "flat 1s ease-out";
+            context.hidden = false;
+            menu.style.animation = "appear 1s ease-out";
+            menu.hidden = false;
+            hideElement(blurTitle);
+        }, 990);
+    });
+}
