72ace6ea13692c5c0431b508e1ec6793f667677a
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2024-12-13 15:40:43 +0800

    mian.html菜单添加

---
+++ kevinServerClientWeb/assets/js/menuContext.js
@@ -0,0 +1,20 @@
+const menuHTML = `
+<div id="links">
+            <div id="navigation">
+                <a class="subTitle" href="introduce.html">介绍</a>
+                <div id="subArea">
+                    <p>导航</p>
+                    <a href="contributors.html">🎈贡献者名单</a>
+                    <a href="usefulLinks.html">🔗有用的链接</a>
+                </div>
+                <div id="subArea">
+                    <p>维护</p>
+                    <a href="codeFormat.html">💻代码格式</a>
+                </div>
+            </div>
+        </div>
+        <a href="main.html">
+            <img id="menuIcon" src="../image/icons/icon.jpg" alt="主页图标">
+        </a>
+</div>
+`;
