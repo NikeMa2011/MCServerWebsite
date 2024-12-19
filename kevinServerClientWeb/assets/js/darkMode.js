257b44b7f429f207c15ca17511ef220c1ea12d97
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2024-12-19 13:13:26 +0800

    添加重要补丁

--- kevinServerClientWeb/assets/js/darkMode.js
+++ kevinServerClientWeb/assets/js/darkMode.js
@@ -1,11 +1,12 @@
 const styleTag = document.getElementById("darkModeStyleTag");
+console.log(styleTag);
 
 function nightModeCompare(op) {
     if (dayNight === "夜晚") {
         styleTag.innerHTML = "@import url(\"../css/nightMode.css\");";
         console.log("夜间模式状态: 开启");
     } else {
-        styleTag.innerHTML = null;
+        styleTag.remove();
         console.log("夜间模式状态: 关闭");
     }
 }
