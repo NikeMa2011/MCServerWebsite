66576d1c3036c0010a4da6505db0ab012c0d127a
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2025-01-21 19:23:11 +0800

    添加重定向快捷键'/'

--- kevinServerClientWeb/assets/js/menu.js
+++ kevinServerClientWeb/assets/js/menu.js
@@ -1,1 +1,7 @@
 document.getElementById("menu").innerHTML = menuHTML;
+
+window.addEventListener("keydown", function(event) {
+    if(event.key == '/') {
+        window.location.href = "main.html";
+    }
+});
