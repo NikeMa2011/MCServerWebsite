51ccdbc37631eebba9414e8f5b4a203c3b8c9388
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2025-01-21 08:49:36 +0800

    去除不必要的变量

--- kevinServerClientWeb/assets/js/darkMode.js
+++ kevinServerClientWeb/assets/js/darkMode.js
@@ -1,6 +1,6 @@
 const styleTag = document.getElementById("darkModeStyleTag");
 
-function nightModeCompare(op) {
+function nightModeCompare() { 
     if (dayNight === "夜晚") {
         styleTag.innerHTML = "@import url(\"../css/nightMode.css\");";
         console.log("夜间模式状态: 开启");
